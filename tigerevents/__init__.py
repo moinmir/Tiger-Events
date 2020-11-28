2ee269205bcbb52db37e4c8a69da84766781fdee
faisal <ffakhro@princeton.edu>
2020-11-28 20:09:57 +0300

    add search bar

--- tigerevents/__init__.py
+++ tigerevents/__init__.py
@@ -2,18 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from sqlalchemy_searchable import make_searchable
 import os
 
 
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/EventsDB"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5432/EventsDB"
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SECRET_KEY'] = "7501d06b12422d9792968f951c600b32"
 
 # Initialize the extensions
 db = SQLAlchemy(app)
+make_searchable()
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
